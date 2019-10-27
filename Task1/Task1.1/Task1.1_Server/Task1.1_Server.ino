bd86c0892d020e41daa46644a789e3a9e2118910
maliq101 <maliq101@gmail.com>
2019-10-27 16:26:38 +0000

    Commit Changes made on the server code
    This changes were made on the 14th October, before end of teaching block

--- Task1/Task1.1/Task1.1_Server/Task1.1_Server.ino
+++ Task1/Task1.1/Task1.1_Server/Task1.1_Server.ino
@@ -1,3 +1,4 @@
+
 /*
 Code modified from Example Arduino Code
  */
@@ -15,8 +16,17 @@ char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                // your network key Index number (needed only for WEP)
 
+int led =  LED_BUILTIN;
+bool client_led=0;
 int status = WL_IDLE_STATUS;
-WiFiServer server(23);
+byte read_mac1;
+char read_mac2;
+int length_of_data;
+byte client_mac_address[6];
+WiFiServer server(80);
+IPAddress client_ip;
+IPAddress sensor_ip(192,168,4,2);//ip address of sensor client
+byte mac[6];//store mac address of connected device
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -26,6 +36,8 @@ void setup() {
   }
 
   Serial.println("Access Point Web Server");
+
+  pinMode(led, OUTPUT);      // set the LED pin mode
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
@@ -79,22 +91,106 @@ void loop() {
       Serial.println("Device disconnected from AP");
     }
   }
-  
   WiFiClient client = server.available();
   if (client.connected()) {                             // if you get a client,
     Serial.println("new client");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.print("Client value read as:");
-        Serial.println(c);                   // print it out the serial monitor
-        client.flush(); 
-        client.stop();
+    Serial.print("Device IP address: ");
+    client_ip=client.remoteIP();//get ip address of connected client
+    Serial.println(client_ip);
+    //Otain MAC Address
+    for(int i=5;i>=0;i--){
+        length_of_data=client.available();
+        byte dummy[length_of_data];
+        int read_data[length_of_data];
+        Serial.println(length_of_data);
+      while(client.available()){
+        for(int i=0; i<length_of_data; i++){
+          dummy[i]=client.read();
+          int j = convert_ansii(dummy[i]);
+          read_data[i]=j;
         }
+        //read_mac2=read_mac1;
+        //Serial.print(read_mac2);
+      }
+      client_mac_address[i]=(read_data[0]*100)+(read_data[1]*10)+read_data[2];
+      Serial.println(client_mac_address[i]);
+    }
+     //int final_value= (read_data[0]*100)+(read_data[1]*10)+read_data[2];
+    //Serial.println(final_value);    
+    Serial.print("MAC address: ");
+    WiFi.BSSID(mac);
+      Serial.print(mac[5],HEX);
+      Serial.print(":");
+      Serial.print(mac[4],HEX);
+      Serial.print(":");
+      Serial.print(mac[3],HEX);
+      Serial.print(":");
+      Serial.print(mac[2],HEX);
+      Serial.print(":");
+      Serial.print(mac[1],HEX);
+      Serial.print(":");
+      Serial.println(mac[0],HEX);   
+    if(client_ip==sensor_ip){//sesnor client connected
+      String currentLine = "";                // make a String to hold incoming data from the client
+      while (client.connected()) {            // loop while the client's connected
+        if (client.available()) {             // if there's bytes to read from the client,
+          char c = client.read();             // read a byte, then
+          Serial.print("Client value read as:");
+          Serial.println(c);                   // print it out the serial monitor
+          client.flush(); 
+          client.stop();
+          Serial.println("Client Disconnected");
+        }
+    }
    }
-   Serial.println("Client Disconnected");
-  }
+   else{//Laptop client connected
+    Serial.println("new client");
+    Serial.println("Laptop client");
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    client.stop();
+    Serial.println("Client Disconnected");
+   }
+   }
+   
 }
 
 void printWiFiStatus() {
@@ -110,5 +206,39 @@ void printWiFiStatus() {
   // print where to go in a browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
-
 }
+
+int sort_out_numbers(int *ptr, int number_length){
+  if(number_length==3){
+    int i=(*ptr*100);
+    ptr++;
+    i+=*ptr*10;
+    ptr++;
+    i+=*ptr;
+    return i;
+  }
+}
+
+int convert_ansii( byte i){
+  switch (i){
+    case 48:
+    return 0;
+    case 49:
+    return 1;
+    case 50:
+    return 2;
+    case 51:
+    return 3;
+    case 52:
+    return 4;
+    case 53:
+    return 5;
+        case 54:
+    return 6;
+    case 55:
+    return 7;
+        case 56:
+    return 8;
+    case 57:
+    return 9;
+  }
